79a25a72b87da35c8078d7987a1d8aeb7325f3bd
rishandyiqbal-hub <rishandyiqbal@gmail.com>
2026-07-29 10:25:56 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+function cape() {
+    document.getElementById("hasil").innerHTML = `
+    <h2>🥺 Peluk Virtual Buat Kamu 🤍</h2>
+
+    <p>
+    Sinii dulu yaa... 🫂🤍
+    <br><br>
+
+    Makasih yaa...
+    Karena walaupun hari ini capee,
+    kamu masih bertahan sampai sejauh ini.
+    Itu bukan hal yang mudah, dan kamu udah melakukan yang terbaik.
+    <br><br>
+
+    Sekarang istirahat dulu yaa...
+    Jangan telat makan 🍜
+    Jangan lupa minum 💧
+    Dan tidur yang nyenyak malam ini. 🌙
+    <br><br>
+
+    Semoga besok harimu jauh lebih ringan,
+    lebih banyak senyumnya,
+    dan lebih banyak hal baik yang datang buat kamu. 🌸🤍
+    </p>
+    `;
+}
+
+function baik() {
+    document.getElementById("hasil").innerHTML = `
+    <h2>🥹 Alhamdulillah 🤍</h2>
+
+    <p>
+    Bacanya aja bikin aku ikut lega. 🌷
+    <br><br>
+
+    Semoga rasa bahagia itu terus menemani kamu hari ini,
+    besok,
+    lusa,
+    dan seterusnya.
+    <br><br>
+
+    Tapi kalau suatu saat nanti kamu capee,
+    jangan dipendem sendirian yaa.
+    <br><br>
+
+    Tetap jaga kesehatan,
+    jangan lupa makan,
+    dan semoga senyum cantik kamu selalu menghiasi harimu. 🤍✨
+    </p>
+    `;
+}
